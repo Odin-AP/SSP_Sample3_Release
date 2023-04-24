9bcf1e83b7d42645c8f967ee8693d7699943466b
Odin-AP <odin.song@adpopcorn.com>
2023-04-24 09:53:16 +0900

    [add] remote 연결

---
+++ SSP_Sample3_release/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSP_Sample3_release
+//
+//  Created by Odin.송황호 on 2023/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
