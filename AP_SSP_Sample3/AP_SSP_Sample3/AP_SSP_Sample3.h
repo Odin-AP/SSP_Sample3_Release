fbc7d0468cb5cbacbb3885061dccf496d3387547
Odin-AP <odin.song@adpopcorn.com>
2023-04-24 14:08:34 +0900

    [add] Framwrok 추가

---
+++ AP_SSP_Sample3/AP_SSP_Sample3/AP_SSP_Sample3.h
@@ -0,0 +1,18 @@
+//
+//  AP_SSP_Sample3.h
+//  AP_SSP_Sample3
+//
+//  Created by Odin.송황호 on 2023/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AP_SSP_Sample3.
+FOUNDATION_EXPORT double AP_SSP_Sample3VersionNumber;
+
+//! Project version string for AP_SSP_Sample3.
+FOUNDATION_EXPORT const unsigned char AP_SSP_Sample3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AP_SSP_Sample3/PublicHeader.h>
+
+
